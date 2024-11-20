1316514e957f5e0af7c463fe88a30a219aeb3422
Arthur Oliveira Braga <arthuroliveirabraga.12@gmail.com>
2024-11-20 20:59:49 -0300

    Port_io update

--- TP3/Port_io/Port_io.vhd
+++ TP3/Port_io/Port_io.vhd
@@ -1,53 +1,68 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_unsigned.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-ENTITY port_io IS
-    PORT (
-        -- INPUT
-        nrst      : IN STD_LOGIC;                      -- RESET
-        clk_in    : IN STD_LOGIC;                      -- CLOCK
-        abus_in   : IN STD_LOGIC_VECTOR(8 DOWNTO 0);   -- ADDRESS INPUT
-        dbus_in   : IN STD_LOGIC_VECTOR(7 DOWNTO 0);   -- DATA TO WRITE
-        wr_en     : IN STD_LOGIC;                      -- ENABLE WRITE
-        rd_en     : IN STD_LOGIC;                      -- ENABLE READ
-        port_io   : INOUT STD_LOGIC_VECTOR(7 DOWNTO 0);-- TWO-WAY PORT
-        
-        -- OUTPUT
-        dbus_out  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)   -- DATA READED
+entity Port_io is
+    generic (
+        port_addr     : STD_LOGIC_VECTOR(8 downto 0) := "000000000"; -- ADDRESS TO port_reg
+        tris_addr     : STD_LOGIC_VECTOR(8 downto 0) := "000000001"; -- ADDRESS TO tris_reg
+        alt_port_addr : STD_LOGIC_VECTOR(8 downto 0) := "000000000"; -- COPYING port_reg
+        alt_tris_addr : STD_LOGIC_VECTOR(8 downto 0) := "000000001"  -- COPYING tris_reg
     );
-END port_io;
+    port (
+        nrst       : in  STD_LOGIC;                      -- RESET
+        clk_in     : in  STD_LOGIC;                      -- CLOCK
+        abus_in    : in  STD_LOGIC_VECTOR(8 downto 0);   -- ADDRESS INPUT
+        dbus_in    : in  STD_LOGIC_VECTOR(7 downto 0);   -- DATA TO WRITE
+        wr_en      : in  STD_LOGIC;                      -- ENABLE WRITE
+        rd_en      : in  STD_LOGIC;                      -- ENABLE READ
+        dbus_out   : out STD_LOGIC_VECTOR(7 downto 0);   -- TWO-WAY PORT
+        port_io    : inout STD_LOGIC_VECTOR(7 downto 0)  -- DATA READED
+    );
+end Port_io;
 
-ARCHITECTURE Behavioral OF port_io IS
-    SIGNAL port_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0'); -- REG DATA OUTPUT
-    SIGNAL tris_reg : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '1'); -- REG DIRECTION CONTROL
-BEGIN
+architecture Behavioral of Port_io is
+    signal port_reg : STD_LOGIC_VECTOR(7 downto 0) := (others => '0'); -- REG DATA OUTPUT
+    signal tris_reg : STD_LOGIC_VECTOR(7 downto 0) := (others => '1'); -- REG DIRECTION CONTROL (1 = in, 0 = out)
+    signal latch    : STD_LOGIC_VECTOR(7 downto 0);                    -- LACTH 
+begin
     -- WRITE port_reg AND tris_reg
-    PROCESS(nrst, clk_in)
-    BEGIN
-        IF nrst = '0' THEN
-            -- RESET
-            port_reg <= (OTHERS => '0');
-            tris_reg <= (OTHERS => '1');
-        ELSIF rising_edge(clk_in) THEN
-            IF wr_en = '1' THEN
-                -- WRITE port_reg
-                IF abus_in(6 DOWNTO 0) = "0000100" THEN
-                    port_reg <= dbus_in;
-                -- WRITE tris_reg
-                ELSIF abus_in(6 DOWNTO 0) = "0001010" THEN
+    process(nrst, clk_in)
+    begin
+        if nrst = '0' then
+            port_reg <= (others => '0');
+            tris_reg <= (others => '1');
+        elsif rising_edge(clk_in) then
+            if wr_en = '1' then
+                if abus_in = tris_addr or abus_in = alt_tris_addr then
                     tris_reg <= dbus_in;
-                END IF;
-            END IF;
-        END IF;
-    END PROCESS;
+                elsif abus_in = port_addr or abus_in = alt_port_addr then
+                    port_reg <= dbus_in;
+                end if;
+            end if;
+        end if;
+    end process;
 
-    -- READ to dbus_out
-    dbus_out <= port_reg WHEN (rd_en = '1' AND abus_in(6 DOWNTO 0) = "0000100") ELSE
-                tris_reg WHEN (rd_en = '1' AND abus_in(6 DOWNTO 0) = "0001010") ELSE
-                (OTHERS => 'Z');
+    -- READ
+    process(rd_en, abus_in, tris_reg, port_reg, port_io)
+    begin
+        if rd_en = '1' then
+            if abus_in = tris_addr or abus_in = alt_tris_addr then
+                dbus_out <= tris_reg;
+            elsif abus_in = port_addr or abus_in = alt_port_addr then
+                latch <= port_io; -- Captura o valor da porta
+                dbus_out <= latch;
+            else
+                dbus_out <= (others => 'Z');
+            end if;
+        else
+            dbus_out <= (others => 'Z');
+        end if;
+    end process;
 
-    -- TWO-WAY port_io
-    port_io <= port_reg WHEN tris_reg = "00000000" ELSE (OTHERS => 'Z');
-
-END Behavioral;
+    -- -- TWO-WAY port_io (Bit to Bit)
+    gen_port_io: for i in 0 to 7 generate
+        port_io(i) <= port_reg(i) when tris_reg(i) = '0' else 'Z';
+    end generate gen_port_io;
+end Behavioral;
